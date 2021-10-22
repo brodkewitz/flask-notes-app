b8377280328881a84978ee6440fd9eafade28bdb
Brandon Rodkewitz <brandon@brphotographs.com>
2021-10-22 09:52:44 -0400

    Add comments to help explain the package structure
    This is still confusing to me, let alone newcomers, since I've only seen
    but never used Python packages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
 
+
+# There are two kinds of "app" here, which is confusing.
+
+# "app" variable is our instance of the Flask class. This gets imported
+# by all of our other modules in the application. This "app" is part of
+# the package.
 app = Flask(__name__)
 
+# "from app" here refers to the whole package, as named by this file's
+# parent directory. Putting our "import" statements down here is a
+# workaround to avoid circular imports, because there are two things
+# called "app". All of the other modules in our application must import
+# the Flask app variable above. Putting imports at the bottom here
+# allows all the modules to have their imports at the top. Yup, it's
+# confusing.
 from app import routes
-
