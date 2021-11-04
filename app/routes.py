81cd235c8339b2394d6888f6a767ea2f1350f4a2
Brandon Rodkewitz <brandon@brphotographs.com>
2021-11-03 22:43:26 -0400

    Add feature to edit existing notes
    Create a new view function for editing existing notes. Reuse the same
    template for creating new notes.

--- app/routes.py
+++ app/routes.py
@@ -52,3 +52,38 @@ def new_note():
         return redirect(url_for('index'))
 
     return render_template("edit.html", title=title, form=form)
+
+
+@app.route("/edit/<int:note_id>", methods=["GET", "POST"])
+def edit_note(note_id):
+    """Edit an existing note"""
+    title = "Edit Note"
+    if not 0 <= note_id < len(notes):
+        flash("No note found with this ID")
+        return redirect(url_for('index'))
+
+    form = EditNoteForm()
+    if form.validate_on_submit():
+        new_note = {
+            "date-modified": datetime.datetime.now(),
+            "title": form.note_title.data,
+            "body": form.note_body.data
+        }
+        notes[note_id] = new_note
+        flash(f"Note updated: {new_note['title']}")
+        return redirect(url_for('index'))
+
+    # The form wasn't submitted at this point, so we're going to render
+    # the form with the existing note's content.
+    # We are preloading the form elements with the note content here,
+    # before sending everything over to the template. Normally we would
+    # send the note_data over to the template, and use it when rendering
+    # the form inputs over there. Unfortunately, WTForms can't
+    # currently do that with a `<textarea>`. So instead of e.g.:
+    # {{ form.note_title(autofocus="", maxlength="80", value=note_data["title"]) }}
+    # ...we're just adding the note data to the form object before it
+    # gets sent over. Luckily this works for both kinds of form inputs.
+    form.note_title.data = notes[note_id]["title"]
+    form.note_body.data = notes[note_id]["body"]
+
+    return render_template("edit.html", title=title, form=form)
