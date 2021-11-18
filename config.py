e7f54438b3193d230f1fd02f91ffc69e6b406fd3
Brandon Rodkewitz <brandon@brphotographs.com>
2021-11-18 18:15:51 -0500

    Add Heroku dependencies and update app config
    Heroku uses PostgreSQL, but generates a database url environment
    variable that SQLAlchemy doesn't like. We just need a simple string
    replacement to fix that up.
    
    Also make sure the app still runs offline. Beef up the logic for
    handling whether the DATABASE_URL environment variable is set. The
    previous code handled this, but now the interpreter will get mad if we
    try to call str.replace() on a nonetype environment variable. The
    behavior ultimately remains the same though - the app will assume we're
    offline and switch over to using the sqlite database if the variable is
    not set.

--- config.py
+++ config.py
@@ -9,6 +9,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess fallback string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Heroku sets a DATABASE_URL environment variable, but SQLAlchemy
+    # doesn't like the url protocol name that is uses. We can fix this
+    # up with a simple string replacement.
+    if 'DATABASE_URL' in os.environ:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+            'postgres://', 'postgresql://')
+    else:
+        # If the environment variable isn't set, assume we're offline
+        # and use the sqlite database.
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
